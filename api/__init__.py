ae9a724ef8bf441a6678353b3a578f7ceb55d891
Humbertzhang <504490160@qq.com>
2017-07-07 22:12:14 +0800

    优化程序

--- api/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-api = Blueprint('api',__name__)
-
-from . import feed,everydaypic,pictures,news,articles,\
-        interactions,profile,comments,light,authentication,user,collect,like
