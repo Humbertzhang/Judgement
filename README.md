5c7b7735c40592098f2f58e91601f157229a2896
Humbertzhang <504490160@qq.com>
2017-07-13 23:38:16 +0800

    post 自动补充json数据

--- README.md
+++ README.md
@@ -3,10 +3,55 @@
 
 ##### 协助Flask api测试
 
+***
+用法：
+python judgement.py
+根据提示输入　api所在文件夹
+程序输出自动生成了测试的api所在的文件名以及改文件下测试了什么api
+生成testByJudgement文件夹和 testByJudgement/test.py文件
+test.py文件下便有自动生成的测试
+
+***
+
+关于自动生成的测试的说明：
+1,若api中有`register`或`signup`字样，生成 def test_a_apiname(self):
+2,若api中有`signin`或`login`字样，生成 def test_b_apiname(self):
+3,其余api生成 def test_c_apiname(self):
+4,对于需要用POST或PUT方法发送 json.dumps({)}的api，会根据get_json().get('content')括号中的内容自动生成json语句.如：'content':'test'
 
 ***
 示例
 ```shell
-python judgement.py
-APIS ADRESS:api
+`python judgement.py`
+APIS ADRESS:`test_apis`
+
+-------------API file Name-----------
+test_apis/forgive.py
+-----------------APIS----------------
+forgive
+
+-------------API file Name-----------
+test_apis/signin.py
+-----------------APIS----------------
+login
+
+-------------API file Name-----------
+test_apis/__init__.py
+-----------------APIS----------------
+
+-------------API file Name-----------
+test_apis/getinfo.py
+-----------------APIS----------------
+get_info
+
+-------------API file Name-----------
+test_apis/signup.py
+-----------------APIS----------------
+signup
+
+-------------API file Name-----------
+test_apis/uploadtime.py
+-----------------APIS----------------
+upload_time
+
 ```
--- judgement.py
+++ judgement.py
@@ -3,144 +3,200 @@ import os
 def Static_Content():
 
 
-	# import things
-	ListOfImport = \
-		["import unittest\n",
-		 "from flask import current_app, url_for,jsonify\n",
-		 "from flask_sqlalchemy import SQLAlchemy\n",
-		 "import random\n",
-		 "import json\n"]
-	ftest.writelines(ListOfImport)
+    # import things
+    ListOfImport = \
+        ["import unittest\n",
+         "from flask import current_app, url_for,jsonify\n",
+         "from flask_sqlalchemy import SQLAlchemy\n",
+         "import random\n",
+         "import json\n"]
+    ftest.writelines(ListOfImport)
 
-	ListOfThings = \
-		["class BasicTestCase(unittest.TestCase):\n\n",
-		 "    def setUp(self):\n",
-		 "        self.app = create_app()\n",
-		 "        self.app_context = self.app.app_context()\n",
-		 "        self.app_context.push()\n",
-		 "        self.client = self.app.test_client()\n",
-		 "        db.create_all()\n\n",
-		 "    def tearDown(self):\n",
-		 "        db.session.remove()\n",
-		 "        db.drop_all()\n",
-		 "        self.app_context.pop()\n\n",
-		 "    def test_app_exist(self):\n",
-		 "        self.assertFalse(current_app is None)\n\n"]
-	ftest.writelines(ListOfThings)
+    ListOfThings = \
+        ["class BasicTestCase(unittest.TestCase):\n\n",
+         "    def setUp(self):\n",
+         "        self.app = create_app()\n",
+         "        self.app_context = self.app.app_context()\n",
+         "        self.app_context.push()\n",
+         "        self.client = self.app.test_client()\n",
+         "        db.create_all()\n\n",
+         "    def tearDown(self):\n",
+         "        db.session.remove()\n",
+         "        db.drop_all()\n",
+         "        self.app_context.pop()\n\n",
+         "    def test_app_exist(self):\n",
+         "        self.assertFalse(current_app is None)\n\n"]
+    ftest.writelines(ListOfThings)
 
 
 def API_Test_Content(api_add):
-	# Open File that need be tested
-	f = open(api_add, 'r')
-	AllLines = f.readlines()
+    # Open File that need be tested
+    f = open(api_add, 'r')
+    AllLines = f.readlines()
 
-	for i in range(len(AllLines)):
-		if "@api.route" in AllLines[i]:
-			count = 0
-			while (True):
-				if "@" in AllLines[i]:
-					count += 1
-					i += 1
-				else:
-					break
+    for i in range(len(AllLines)):
+        if "@api.route" in AllLines[i]:
+            count = 0
+            while (True):
+                if "@" in AllLines[i]:
+                    count += 1
+                    i += 1
+                else:
+                    break
 
-			index1 = AllLines[i].find("def")
-			index2 = AllLines[i].find("(")
-			str = AllLines[i][index1 + 4:index2]
+            index1 = AllLines[i].find("def")
+            index2 = AllLines[i].find("(")
+            str = AllLines[i][index1 + 4:index2]
             
-			print str
+            print str
 
-			# special api
-			if "register" in str or "signup" in str:
-				ftest.writelines([
+            # special api
+            if "register" in str or "signup" in str:
+                ftest.writelines([
                                     "    #Test " + str,
                                     "\n    def test_a_" + str + "(self):\n"
                                 ])
 
-			elif "signin" in str or "login" in str:
-				ftest.writelines([
+            elif "signin" in str or "login" in str:
+                ftest.writelines([
                                     "    #Test "+str,
                                     "\n    def test_b_" + str + "(self):\n"
                                 ])
 
-			else:
-				ftest.writelines([
+            else:
+                ftest.writelines([
                                     "    #Test "+str,
                                     "\n    def test_c_" + str + "(self):\n"
                                 ])
-			###End special api
+            ###End special api
 
 
-			# find out methods
-			if "POST" in AllLines[i - count]:
-				ftest.writelines(
-					["        response = self.client." + "post" + "(\n"]
-				)
+            # find out methods
+            if "POST" in AllLines[i - count]:
+                ftest.writelines(
+                    ["        response = self.client." + "post" + "(\n",
+                     "            url_for('api." + str + "',_external=True),\n",
+                     "            data = json.dumps({\n"])
+                
+                #POST json data
+                j=0
+                while("return" not in AllLines[j+i-count]):
+                    if "get_json()" in AllLines[j+i-count]:
+                        
+                        index1 = AllLines[j+i-count].find("().get")
+                        
+                        if "\"" in AllLines[j+i-count][index1+8:-1]:
+                            index2 = AllLines[j+i-count][index1+8:-1].find("\"")+index1+8
+                        elif "\'" in AllLines[j+i-count][index1+8:-1]:
+                            index2 = AllLines[j+i-count][index1+8:-1].find("\'")+index1+8
+                        else:
+                            index2 = -1
+                        attr = AllLines[j+i-count][index1+8:index2]
+                        ftest.writelines([" "*16 +"\"" + attr + "\":'test',\n"])
+                    j+=1
+                           
 
-				ftest.writelines(
-					["            url_for('api." + str + "',_external=True),\n",
-					 "            data = json.dump({ #Need complete}),\n",
-					 "            content_type = 'application/json')\n",
-					 "        self.assertTrue(response.status_code == 200)\n"]
-				)
+                #End of post json data
+                ftest.writelines(
+                    ["            }),\n",
+                     "            content_type = 'application/json')\n",
+                     "        self.assertTrue(response.status_code == 200)\n\n"])
+                
+                
+            elif "PUT" in AllLines[i - count]:
+                ftest.writelines(
+                    ["        response = self.client." + "put" + "(\n",
+                     "            url_for('api." + str + "',_external=True),\n",
+                     "            data = json.dumps({\n"])
+                
+                #PUT json data
+                j=0
+                while("return" not in AllLines[j+i-count]):
+                    if "get_json()" in AllLines[j+i-count]:
+                        
+                        index1 = AllLines[j+i-count].find("().get")
+                        if "\"" in AllLines[j+i-count][index1+8:-1]:
+                            index2 = AllLines[j+i-count][index1+8:-1].find("\"")+index1+8
+                        elif "\'" in AllLines[j+i-count][index1+8:-1]:
+                            index2 = AllLines[j+i-count][index1+8:-1].find("\'")+index1+8
+                        
+                        attr = AllLines[j+i-count][index1+8:index2]
+                        ftest.writelines([" "*16 +"\"" + attr + "\":'test',\n"])
+                    j+=1
+                           
 
-			elif "GET" in AllLines[i - count]:
-				ftest.writelines(
-					["        response = self.client." + "get" + "(\n"]
-				)
+                #End of post json data
+                ftest.writelines(
+                    ["            }),\n",
+                     "            content_type = 'application/json')\n",
+                     "        self.assertTrue(response.status_code == 200)\n\n"])
+                
+            
+            elif "DELETE" in AllLines[i - count]:
+                ftest.writelines(
+                    ["        response = self.client." + "delete" + "(\n",
+                     "            url_for('api." + str + "',_external=True),\n",
+                     "            content_type = 'application/json')\n",
+                     "        self.assertTrue(response.status_code == 200)\n\n"]
+                )
+            elif "GET" in AllLines[i - count]:
+                ftest.writelines(
+                    ["        response = self.client." + "get" + "(\n",
+                     "            url_for('api." + str + "',_external=True),\n",
+                     "            content_type = 'application/json')\n",
+                     "        self.assertTrue(response.status_code == 200)\n\n"]
+                )
 
-				ftest.writelines(
-					["            url_for('api." + str + "',_external=True),\n",
-					 "            content_type = 'application/json')\n",
-					 "        self.assertTrue(response.status_code == 200)\n"]
-				)
-			else:
-				ftest.writelines(
-					["        response = self.client." + "XXXX" + "(\n"]
-				)
-				ftest.writelines(
-					["            url_for('api." + str + "',_external=True),\n",
-					 "            content_type = 'application/json')\n",
-					 "        self.assertTrue(response.status_code == 200)\n"]
-				)
-			# End Of find out methods
-			f.close()
+            else:
+                ftest.writelines(
+                    ["        response = self.client." + "XXXX" + "(\n"]
+                )
+                ftest.writelines(
+                    ["            url_for('api." + str + "',_external=True),\n",
+                     "            content_type = 'application/json')\n",
+                     "        self.assertTrue(response.status_code == 200)\n\n"]
+                )
+            
+            # End Of find out methods
+            f.close()
 
 
 if __name__ == "__main__":
-	API_Floder = raw_input("API Floder Address:")
-	files = os.listdir(API_Floder)
+    API_Floder = raw_input("API Floder Address:")
+    files = os.listdir(API_Floder)
 
 
-	# new Open Test File
-	if os.path.isdir("testByJudgement"):
-		pass
-	else:
-		os.mkdir("testByJudgement")
+    # new Open Test File
+    if os.path.isdir("testByJudgement"):
+        pass
+    else:
+        os.mkdir("testByJudgement")
 
-	if os.path.isfile("testByJudgement/test.py"):
-		pass
-	else:
-		os.mknod("testByJudgement/test.py")
+    if os.path.isfile("testByJudgement/test.py"):
+        pass
+    else:
+        os.mknod("testByJudgement/test.py")
 
-	ftest = open("./testByJudgement/test.py", "w+")
-	#End Open Test File
-	apis = []
-	for i in range(len(files)):
-		if '.py' in files[i] and '.pyc' not in files[i]:
-			apis.append(files[i])
+    ftest = open("./testByJudgement/test.py", "w+")
+    #End Open Test File
+    apis = []
+    for i in range(len(files)):
+        if '.py' in files[i] and '.pyc' not in files[i]:
+            apis.append(files[i])
 
 
-	file_addes = []
-	for i in range(len(apis)):
-		file_addes.append(API_Floder +'/'+apis[i])
+    file_addes = []
+    for i in range(len(apis)):
+        file_addes.append(API_Floder +'/'+apis[i])
 
-	Static_Content()
+    Static_Content()
 
-	for i in range(len(file_addes)):
-		print '\n-------------API file Name-----------'
-		print file_addes[i]
-		print '-----------------APIS----------------'
-		API_Test_Content(file_addes[i])
 
-	ftest.close()
+    for i in range(len(file_addes)):
+        print '\n-------------API file Name-----------'
+        print file_addes[i]
+        ftest.writelines(["\n"+" "*4 + "#--------API FILE NAME:"+ file_addes[i] + "------------------------------------------\n\n"])
+        print '-----------------APIS----------------'
+        API_Test_Content(file_addes[i])
+
+    ftest.close()
